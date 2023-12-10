0a4b37061abd287f223209da22999a83b9b059d2
chanthychongkam <chanthy.chongkam@projectarchie.com>
2023-12-10 16:42:51 +1100

    change label

--- apra.py
+++ apra.py
@@ -4,4 +4,4 @@ st.title("Project Archie - APRA POC")
 st.write('Hello Archie version 3')
 
 with st.sidebar:
-    vABN= st.text_input(label='ABN')
+    vABN= st.text_input(label='Please provide your ABN')
