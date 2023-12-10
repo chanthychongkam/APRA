39063a6471788d722856b332c52058344ab8e321
chanthychongkam <chanthy.chongkam@projectarchie.com>
2023-12-10 16:35:26 +1100

    sidebar

--- apra.py
+++ apra.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.title("Project Archie - APRA POC")
 st.write('Hello Archie version 3')
 
-st.sidebar
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
