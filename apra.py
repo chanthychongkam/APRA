3b5642237e64fd69287dfc31e5d3cf92a813c845
chanthychongkam <chanthy.chongkam@projectarchie.com>
2023-12-10 16:40:20 +1100

    abn

--- apra.py
+++ apra.py
@@ -3,7 +3,4 @@ import streamlit as st
 st.title("Project Archie - APRA POC")
 st.write('Hello Archie version 3')
 
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
+vABN= st.text_input(label='ABN')
